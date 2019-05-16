bd4ac964d87cca6eb025ffc0279b00031c6b5381
Kazeia <davidsilva3290@gmail.com>
2019-05-16 01:58:29 -0500

    TODO

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using IronPdf;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace BrancaBRRRRRRR_LOSILUMINATI_BRRRRR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Select one or more images.  This example selects all JPEG images in a specific folder.
+            var ImageFiles = Directory.EnumerateFiles(@"C:\Users\kaz\source\repos\Branca\BrancaBRRRRRRR_LOSILUMINATI_BRRRRR").Where(f => f.EndsWith(".jpg") || f.EndsWith(".jpeg"));
+            // Convert the images to a PDF and save it.
+            ImageToPdfConverter.ImageToPdf(ImageFiles).SaveAs(@"C:\Users\kaz\source\repos\Branca\BrancaBRRRRRRR_LOSILUMINATI_BRRRRR\composite.pdf");
+
+            //Also see PdfDocument.RasterizeToImageFiles() method to flatten a PDF to images or thumbnails
+
+            Console.ReadLine();
+        }
+    }
+}
